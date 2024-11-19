edda979ec4fe1afdc6d5f5d45054a734065481eb
JallsBR <oswaldojales@gmail.com>
2024-11-19 11:38:10 -0300

    upgrade Section - content contenier

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,25 +1,49 @@
-import  {Container, Links} from './styles' 
-import {Button } from '../../components/Button'
+import  {Container, Links, Content} from './styles' 
+import {Button} from '../../components/Button'
+import {ButtonText} from '../../components/ButtonText'
 import {Header} from '../../components/Header' 
 import { Section } from '../../components/Section'
-
+import { Tag } from '../../components/Tag'
+ 
 export function Details() {
   return (
-  <Container>
+  <Container> 
       <Header />
 
-      <Section title="Línks Úteis">
-        <Links>
-          <li>
-            <a href="https://rocketseat.com.br">rocketseat.com.br</a>
-          </li>
-          <li>
-            <a href="https://github.com/rocketseat">github.com/rocketseat</a>
-          </li>
-        </Links>
-      </Section>
+      <main>
+        <Content>
+          
+          <ButtonText title="Excluir Nota" />
 
-      <Button title="Voltar"/>  
+          <h1> Introdução ao React </h1>
+          <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. A doloremque minus molestias et? Doloribus, hic facilis! 
+            Quaerat quis doloribus accusamus facilis temporibus culpa nobis voluptate repellat nostrum. Repellat, recusandae deleniti!
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. A doloremque minus molestias et? Doloribus, hic facilis! 
+            Quaerat quis doloribus accusamus facilis temporibus culpa nobis voluptate repellat nostrum. Repellat, recusandae deleniti!
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. A doloremque minus molestias et? Doloribus, hic facilis! 
+            Quaerat quis doloribus accusamus facilis temporibus culpa nobis voluptate repellat nostrum. Repellat, recusandae deleniti!</p>
+
+          <Section title="Línks Úteis">
+            <Links>
+              <li>
+                <a href="https://rocketseat.com.br">rocketseat.com.br</a>
+              </li>
+              <li>
+                <a href="https://github.com/rocketseat">github.com/rocketseat</a>
+              </li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tag title="React"/>
+            <Tag title="Node.js"/>
+          </Section>
+
+          <Button title="Voltar"/>  
+
+        </Content>
+      </main>
+
   </Container>
 
   )
